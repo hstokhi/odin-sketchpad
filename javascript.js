38cceb9cdce20c6c6a50dc81fcec2de62b23dad8
Hakim Tokhi <hstokhi@yahoo.com>
2023-05-08 23:16:45 -0700

    Implement DOM changes on 'mouseover' event
    Made adjustments to makeGrid function to add 'colored'
    class to elements of grid when mouse hovers over div.

--- javascript.js
+++ javascript.js
@@ -1,19 +1,35 @@
-//Pseudocode - create 16 rows of 16 divs. each row wrapped in a div with display:flex//
+//create 16 rows of 16 divs. each row wrapped in a div with display:flex//
 const container = document.querySelector('.container')
-const row = document.createElement('div')
-row.classList.add('row')
-row.style.display = 'flex'
-const div = document.createElement('div')
 
 function makeGrid(gridSize) {
+    const size = (1/gridSize)*100
     for (let i = 0; i < gridSize; i++) {
+        const row = document.createElement('div')
+        row.classList.toggle('row')
+        row.style.display = 'flex'
         container.appendChild(row)
+        row.style.height = `${size}vh`
         for (let j = 0; j < gridSize; j++) {
+            const div = document.createElement('div')
+            div.classList.toggle('column')
+            div.style.width = `${size}vw`
+            div.addEventListener('mouseover', () => {
+                div.classList.add('colored')
+            })
             row.appendChild(div)
         }
     }
+    
+    
 }
 
 window.onload = function() {
     makeGrid(16)
-}
+};
+
+/*const grid = document.querySelectorAll('.column');
+grid.forEach((gridBlock) => {
+    gridBlock.addEventListener('mouseover', () => {
+        gridBlock.classList.toggle('colored')
+    });
+});*/
